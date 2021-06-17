645a6c0ed0f26bc215c06e7d763f632e42c09250
Muhammad Talha <muhammadtalha3810@gmail.com>
2021-06-18 02:08:26 +0500

    White list

--- init.js
+++ init.js
@@ -32,7 +32,7 @@ module.exports.init = async function init() {
         app.use(cookieParser());
         app.use(helmet());
         const cors = function (req, res, next) {
-            const whitelist = ['localhost:3000', 'http://localhost:3000', 'http://localhost:3001', 'http://localhost:3002', 'http://20.101.44.78:3003/'];
+            const whitelist = ['localhost:3000', 'http://localhost:3000', 'http://localhost:3001', 'http://localhost:3002', 'http://20.101.44.78:3003/', '120.101.44.78:3003', 'http://20.101.44.78:3003', 'http://20.101.44.78'];
             const origin = req.headers.origin;
             const host = req.headers.host;
             if (whitelist.indexOf(origin) > -1 || whitelist.indexOf(host) > -1) {
